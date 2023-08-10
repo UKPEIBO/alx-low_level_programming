1522597dce3bafa71b2a9431c9141a255fe6a8f7
UKPEIBO <jasmineejirogift468@gmail.com>
2023-08-10 09:59:50 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ * main - Entery point
  *
- *Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
