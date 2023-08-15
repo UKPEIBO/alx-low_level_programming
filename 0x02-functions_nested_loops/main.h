3f23206c933fa68f773f5dbdff89767c3da7ab4c
UKPEIBO <jasmineejirogift468@gmail.com>
2023-08-15 08:58:45 -0700

    file1 and file 2 done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _putcher(int c);
+int _putchar(int c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,4 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-~
