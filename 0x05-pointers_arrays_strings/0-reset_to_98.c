1c6c145782ad809572732edf62ad9bc278789546
UKPEIBO <jasmineejirogift468@gmail.com>
2023-08-21 15:11:15 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function resets the value of
+ * an int to 98
+ *
+ * @n: the value of an indirection pointer
+ *
+ * Return: nth
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
